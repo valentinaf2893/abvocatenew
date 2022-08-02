43804a1040380a116053641b7bf33daf1ad456df
Valentina Flores <valentinaflores2893@gmail.com>
2022-08-02 17:06:52 -0400

    metadata!

---
+++ testingscrollthing/MetadataTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MetadataTableViewCell.swift
+//  testingscrollthing
+//
+//  Created by Scholar on 8/2/22.
+//
+
+import UIKit
+
+class MetadataTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var metaDataImage: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ testingscrollthing/NewsPageController.swift
@@ -0,0 +1,97 @@
+//
+//  NewsPageController.swift
+//  testingscrollthing
+//
+//  Created by Scholar on 8/2/22.
+//
+
+import UIKit
+
+class NewsPageController: UITableViewController {
+    
+    var articles : [AddArticle] = []
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        articles = addArticles()
+    }
+    
+    func addArticles() -> [AddArticle] {
+        let first = AddArticle()
+        first.articleLink = "https://www.nature.com/articles/d41586-022-01775-z"
+        first.new = true
+        
+        return [first]
+    }
+
+    // MARK: - Table view data source
+
+//    override func numberOfSections(in tableView: UITableView) -> Int {
+//        // #warning Incomplete implementation, return the number of sections
+//        return 0
+//    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return articles.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        let newOrOld = articles[indexPath.row]
+        
+        if newOrOld.new {
+            cell.textLabel?.text = "😀"+newOrOld.articleLink
+        } else {
+            cell.textLabel?.text = newOrOld.articleLink
+        }
+        return cell
+    }
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
