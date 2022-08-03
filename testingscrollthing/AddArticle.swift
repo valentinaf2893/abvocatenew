4cafde945e3e4fd1cdda2a178388d8ded7759848
Shrutee <shruteebehera@gmail.com>
2022-08-03 10:01:16 -0400

    Initial Commit

---
+++ testingscrollthing/AddArticle.swift
@@ -0,0 +1,14 @@
+//
+//  AddArticle.swift
+//  testingscrollthing
+//
+//  Created by Scholar on 8/2/22.
+//
+
+import UIKit
+
+class AddArticle {
+    var articleLink = " "
+    var new = false
+    var image = ""
+}
